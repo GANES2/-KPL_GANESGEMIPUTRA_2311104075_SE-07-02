b3a7dbe7029af3e1c77857ee33887beccb9daa4e
GANES2 <168392728+GANES2@users.noreply.github.com>
2025-03-04 11:47:18 +0700

    Add files via upload

---
+++ 03_GUI_Builder_dan_GitHub /State-Based Construction/DoorMachine.cs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class DoorMachine
+{
+    public enum State { Terkunci, Terbuka }
+    private State state;
+
+    public DoorMachine()
+    {
+        state = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void Unlock()
+    {
+        if (state == State.Terkunci)
+        {
+            state = State.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+
+    public void Lock()
+    {
+        if (state == State.Terbuka)
+        {
+            state = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+    }
+}
