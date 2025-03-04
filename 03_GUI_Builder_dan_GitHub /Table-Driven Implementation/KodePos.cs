3c77975d76d76b1e720433806a9651049ba940a6
GANES2 <168392728+GANES2@users.noreply.github.com>
2025-03-04 11:44:30 +0700

    Add files via upload

---
+++ 03_GUI_Builder_dan_GitHub /Table-Driven Implementation/KodePos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class KodePos
+{
+    private static Dictionary<string, string> kodePosDict = new Dictionary<string, string> {
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"}
+    };
+
+    public static string GetKodePos(string kelurahan)
+    {
+        return kodePosDict.ContainsKey(kelurahan) ? kodePosDict[kelurahan] : "Kode pos tidak ditemukan";
+    }
+}
---
+++ 03_GUI_Builder_dan_GitHub /Table-Driven Implementation/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Kode Pos Batununggal: " + KodePos.GetKodePos("Batununggal"));
+        Console.WriteLine("Kode Pos Wates: " + KodePos.GetKodePos("Wates"));
+    }
+}
+
+
+
